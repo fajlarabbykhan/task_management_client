ae6dc2a445cfe6125c919ad3868d840606a5df80
fajlarabbykhan <fajlarabbyk55@gmail.com>
2022-07-01 21:40:57 +0600

    40min

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Register from "./Pages/Register";
 import Welcome from "./Pages/Welcome";
+export const CredentialContext = React.createContext()
 function App() {
+  const credentialState = useState(null)
   return (
     <div className="">
-      <Routes>
+      <CredentialContext.Provider value={credentialState}>
+        <Routes>
 
-        <Route path="/" element={<Welcome />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </CredentialContext.Provider>
+
     </div>
   );
 }
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom"
-
+import { Link, useNavigate } from "react-router-dom";
+const handleErrors = async (response) => {
+    if (!response.ok) {
+        const { message } = await response.json()
+        throw Error(message)
+    }
+    return response.json();
+};
 const Register = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
 
     const register = (e) => {
         e.preventDefault();
@@ -18,15 +25,30 @@ const Register = () => {
                 password,
             })
         })
+            .then(handleErrors)
+            .then(() => {
+                navigate('/')
+
+            })
+            .catch((error) => {
+                setError(error.message);
+            })
     }
+
+    const navigate = useNavigate()
+
 
     return (
         <div className=' text-center bg-[#1a0d26e6] w-full h-screen fixed py-5 '>
             <h2 className='text-2xl text-white'>Register Now </h2>
+
             <div className="hero min-h-screen">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="card flex-shrink-0 w-full max-w-sm  shadow-2xl bg-base-100">
                         <div className="card-body ">
+                            {
+                                error && (<span style={{ color: 'red' }}>{error}</span>)
+                            }
                             <form onSubmit={register} className='place-items-center'>
                                 <input onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Your email please" className="input input-bordered m-5 border-rose-600 p-5" />
                                 <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Create password" className="input input-bordered border-rose-600 m-5 p-5" />
